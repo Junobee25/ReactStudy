3a309796ade0dca2a7d2d109e2272ddb4578be7d
Junobee25 <wnsdhqo@naver.com>
2023-01-12 01:17:13 +0900

    Feat - 더보기 기능 추가

--- boilerplate-mern-stack-master/client/src/components/views/LandingPage/LandingPage.js
+++ boilerplate-mern-stack-master/client/src/components/views/LandingPage/LandingPage.js
@@ -1,63 +1,85 @@
-import React, { useEffect,useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { FaCode } from "react-icons/fa";
 import axios from "axios";
-import {Icon,Col,Card,Row,Carousel} from 'antd';
-import Meta from 'antd/lib/card/Meta';
-import ImageSlider from '../../utils/ImageSlider';
-
-
+import { Icon, Col, Card, Row, Carousel } from "antd";
+import Meta from "antd/lib/card/Meta";
+import ImageSlider from "../../utils/ImageSlider";
 
 function LandingPage() {
+  const [Products, setProducts] = useState([]);
+  const [Skip, setSkip] = useState(0);
+  const [Limit, setLimit] = useState(8);
+  const [PostSize, setPostSize] = useState(0);
 
-    const [Products,setProducts] = useState([])
+  useEffect(() => {
+    let body = {
+      skip: Skip,
+      limit: Limit,
+    };
 
+    getProducts(body);
+  }, []);
 
-    useEffect(()=>{
-        
+  const getProducts = (body) => {
+    axios.post("/api/product/products", body).then((response) => {
+      if (response.data.success) {
+        if (body.loadMore) {
+          setProducts([...Products, ...response.data.productInfo]);
+        } else {
+          setProducts(response.data.productInfo);
+        }
+        setPostSize(response.data.postSize)
+      } else {
+        alert("상품들을 가져오는데 실패 했습니다.");
+      }
+    });
+  };
 
-        axios.post('/api/product/products')
-            .then(response=>{
-                if (response.data.success){
-                    setProducts(response.data.productInfo)
-                }else{
-                    alert("상품들을 가져오는데 실패 했습니다.")
-                }
-            })
+  const loadMoreHandler = () => {
+    let skip = Skip + Limit;
 
-    },[])
+    let body = {
+      skip: skip,
+      limit: Limit,
+      loadMore: true,
+    };
 
-    const renderCards = Products.map((product,index)=>{
-        console.log(product)
-        return <Col lg={6} md={8} xs={24} key={index}>
-            <Card 
-                cover={<ImageSlider images={product.images}/>}>
-            <Meta title={product.title}
-                        description={`${product.price}`}/>
-            </Card>
-        </Col>
-    })
+    getProducts(body);
+    setSkip(skip);
+  };
+  const renderCards = Products.map((product, index) => {
+    console.log(product);
     return (
-        <div style={{width:'75%',margin:'3rem auto'}}>
-            <div style={{textAlign:'center'}}>
-                <h2>Let's Travel Anywhere<Icon type='rocket'/></h2>
-            </div>
-            {/** Filter */}
-            {/** Search */}
-            {/** Card */}
+      <Col lg={6} md={8} xs={24} key={index}>
+        <Card cover={<ImageSlider images={product.images} />}>
+          <Meta title={product.title} description={`${product.price}`} />
+        </Card>
+      </Col>
+    );
+  });
+  return (
+    <div style={{ width: "75%", margin: "3rem auto" }}>
+      <div style={{ textAlign: "center" }}>
+        <h2>
+          Let's Travel Anywhere
+          <Icon type="rocket" />
+        </h2>
+      </div>
+      {/** Filter */}
+      {/** Search */}
+      {/** Card */}
 
-            <Row gutter={[16,16]}>
-                {renderCards}
-            </Row>
+      <Row gutter={[16, 16]}>{renderCards}</Row>
 
-            
-
-          
-
-            <div style={{display:'flex',justifyContent:'center'}}>
-                <button>더보기</button>
-            </div>
+      {PostSize>=Limit &&
+        <div style={{ display: "flex", justifyContent: "center" }}>
+            <button onClick={loadMoreHandler}>더보기</button>
         </div>
-    )
+      }
+
+      
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
